3761a6dd15ca042685b6063a662666b951407c0c
naman9896 <namanmattu36166@gmail.com>
2025-12-09 13:11:36 +1100

    fixed menu with TextLoop

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 z-50">
       <div>
         <a href="/">
           <img src={Logo} alt="Logo Image" className="logo" />
